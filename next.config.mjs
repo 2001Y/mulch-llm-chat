5e4da4378887ca722931e41a6db291f832a6d2cc
2001Y <15167896+2001Y@users.noreply.github.com>
2024-07-10 07:45:22 +0000

    add ALl

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/manifest.json',
+        destination: '/manifest.json.js'
+      }
+    ];
+  }
 };
 
 export default nextConfig;
